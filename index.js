a48cbb7fca73c2856c809eeb65dc82b13cf5dbd4
JW <89119177+wjiasen@users.noreply.github.com>
2025-08-09 12:26:23 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
 const app = express();
-const orderRoutes = require('./routes/order');
-
-app.use(express.json());
-app.use('/api', orderRoutes);
-
 const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// health check
+app.get("/", (req, res) => res.send("Knight Logistics API Running"));
+
+// routes
+const orderRoutes = require("./routes/order");
+app.use("/api/order", orderRoutes); // => POST https://.../api/order
+
+// start
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
